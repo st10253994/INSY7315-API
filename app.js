0fa3a072ecb7b1e9d4f814db03a2506e911823a0
ST10255814 <ST10255814@vcconnect.edu.za>
2025-09-02 17:14:32 +0200

    Implemented CORS

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const app = express();
 const { connectMongo } = require('./database/db');
 const { checkAuth } = require('./Auth/checkAuth')
 const { upload } = require('./database/cloudinary');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json()); // Parse JSON bodies
+app.use(cors())
 
 // Simple test route
 app.get('/', (req, res) => {
