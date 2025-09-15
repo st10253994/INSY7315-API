b227fb1926476f3e9b159c051e2ace089412dd0f
ST10255814 <ST10255814@vcconnect.edu.za>
2025-09-15 16:11:26 +0200

    Updated the get bookings to fetch via userId

--- service/bookingService.js
+++ service/bookingService.js
@@ -93,7 +93,7 @@ async function getBookingById(id) {
   const db = client.db('RentWise');
   const bookings = db.collection('Bookings');
 
-  const booking = await bookings.findOne({ _id: toObjectId(id) });
+  const booking = await bookings.findOne({ userId: toObjectId(id) });
   if (!booking) throw new Error('No Booking Was Found');
   
   return booking;
