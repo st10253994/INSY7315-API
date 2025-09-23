3a6755279a168623b7dac185edb62ec89bec9321
Snoxx <ST10253994@vcconnect.edu.za>
2025-09-23 12:23:14 +0200

    Add userSettings collection reference in googleSignInService constructor and insert user data into User-Settings

--- service/googleSignInService.js
+++ service/googleSignInService.js
@@ -13,6 +13,7 @@ class googleSignInService {
   constructor() {
     const db = client.db('RentWise');
     this.collection = db.collection('System-Users');
+    const userSettings = db.collection('User-Settings');
   }
 
   //Find user by Google ID
@@ -27,7 +28,8 @@ class googleSignInService {
       createdAt: new Date(),
     };
     const result = await this.collection.insertOne(user);
-    return { _id: result.insertedId, ...user };
+    const insert = await userSettings.insertOne(user);
+    return { _id: result.insertedId,_id: insert.insertedId, ...user };
   }
 
 
